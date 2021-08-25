a0d480033e7c6c17d8e0ac2051578aa308f5b3ab
pikoolas <31520568+pikoolas@users.noreply.github.com>
2021-08-25 15:12:39 +0200

    Delete src directory

--- src/IO.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdlib.h>
-
-#ifndef IO_H
-#define IO_H
-
-int read_graph(char* gfile,  int **xadj, int **adj, double **ewghts, int **vwghts, int* nov, int loop);
-
-#endif
