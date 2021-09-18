73824ae5ff2c287cad8f1174b1d4c12ce3f38b6b
mahtabnik-polito <62258209+mahtabnik-polito@users.noreply.github.com>
2021-09-18 02:02:35 +0200

    fixed issues

--- luby/luby.cpp
+++ luby/luby.cpp
@@ -8,13 +8,13 @@
 #include <chrono>
 #include <sstream>
 #include <vector>
+#include <set>
+#include <mutex>
 
 using namespace std;
+std::mutex mu;
 
-int NTHREAD;
-
-class Graph
-{
+class Graph {
     int V;
     int vs;
     vector<int> *graph;
@@ -24,222 +24,203 @@ class Graph
 public:
     Graph();
 
-    Graph( int V )
-    {
+    Graph(int V) {
         this->V = V;
         this->graph = new vector<int>[this->V + 1];
         this->M_counter = 0;
         this->vs = this->V;
     }
 
-    ~Graph()
-    {
+    ~Graph() {
         delete[] graph;
     }
 
-    void addEdge( int x, int y )
-    {
-        this->graph[ x ].push_back( y );
+    void addEdge(int x, int y) {
+        this->graph[x].push_back(y);
     }
 
-    void print()
-    {
-        for ( int i = 1; i <= V; i++ )
-        {
+    void print() {
+        for (int i = 1; i <= V; i++) {
             cout << i << " --> ";
 
-            for ( int nbr : graph[ i ] )
+            for (int nbr : graph[i])
                 cout << nbr << ", ";
 
             cout << endl;
         }
     }
 
-    vector<int> FindMaximalIndependentSet()
-    {
+    vector<int> FindMaximalIndependentSet() {
+        //initialization of I
+        //vector<int> A[] = graph;
         int _V = vs;
+//        int _V = 0;
+        int M_counter = 0;
         vector<int> *A = new vector<int>[V + 1];
         vector<int> M;
-        mutex m;
 
-        for ( int i = 1; i <= V; i++ )
-            for ( int j = 0; j < graph[ i ].size(); j++ )
-                A[ i ].push_back( graph[ i ][ j ] );
+        for (int i = 1; i <= V; i++)
+            for (int j = 0; j < graph[i].size(); j++)
+                A[i].push_back(graph[i][j]);
 
-        int duration = V / NTHREAD;
-        if ( NTHREAD > V )
-        {
-            duration = 1;
-            NTHREAD = V;
-        }
-
-        vector<int> threads_size( NTHREAD, duration );
-
-        // assigning remaining vertex to the corresponding thread
-        if ( NTHREAD * duration < V )
-        {
-            int n = V - ( NTHREAD * duration );
-            for ( int i = 0; i < n; i++ )
-                threads_size[ i ] ++;
-        }
-
-        std::vector<std::thread> threadPool;
-        while ( _V > 0 )
-        {
+        while (_V > 0) {
             /*cout << endl << "print" << endl;
             for ( int i = 1; i <= V; i++ )
             {
                 cout << i << " --> ";
+
                 for ( int nbr : A[ i ] )
                     cout << nbr << ", ";
+
                 cout << endl;
             }*/
 
             //firstly choose a random set of V with the probability of 1/d(v)
-            vector<double> randomValues( V + 1 );
-
-            int end = 1;
-            for ( int i = 0; i < NTHREAD; i++ )
-            {
-                int start = end;
-                end += threads_size[ i ];
-
+            vector<double> randomValues(V + 1);
+            //random values
+            std::vector<std::thread> threadPool;
+            for (int i = 0; i < 8; i++) {
+                int duration = V / 8;
+                int start = i * duration;
+                int end = (i + 1) * duration;
                 threadPool.emplace_back(
                         thread(
-                                [ start, end, A, &randomValues ]()
-                                {
-                                    for ( int v = start; v < end; v++ )
-                                    {
-                                        if ( A[ v ].size() == 0 )
-                                            randomValues[ v ] = 0;
+                                [start, end, A, &randomValues]() {
+
+                                    for (int v = start; v <= end; v++) {
+                                        if (A[v].size() == 0)
+                                            randomValues[v] = 0;
                                         else
-                                            randomValues[ v ] = ( double ) 1 / ( 2 * A[ v ].size() );
+                                            randomValues[v] = (double) 1 / (2 * A[v].size());
                                     }
+
+
                                 }
                         )
                 );
             }
-
-            for ( auto &t : threadPool )
+            for (auto &t : threadPool)
                 t.join();
 
             threadPool.clear();
 
-            vector<int> M_prim;
+            set<int> M_prim;
 
+            std::vector<std::thread> threadPool2;
             //if any of them are neighbors, remove the one with the lowest probability
-            end = 1;
-            for ( int i = 0; i < NTHREAD; i++ )
-            {
-                int start = end;
-                end += threads_size[ i ];
+            for (int i = 0; i < 8; i++) {
+                int duration = V / 8;
+                int start = i * duration;
+                int end = (i + 1) * duration;
 
-                threadPool.emplace_back(
+                threadPool2.emplace_back(
                         thread(
-                                [ start, end, A, randomValues, &M_prim, &m ]()
-                                {
-                                    for ( int v = start; v < end; v++ )
-                                    {
-                                        if ( A[ v ].size() == 0 )
-                                            continue;
+                                [start, end, A, randomValues, &M_prim]() {
 
-                                        bool add = true;
+                                    for (int v = start; v <= end; v++) {
 
-                                        for ( int u = 0; u < A[ v ].size(); u++ )
-                                        {
-                                            if ( randomValues[ v ] <= randomValues[ A[ v ][ u ]] )
-                                            {
-                                                add = false;
-                                                break;
+                                        for (int u = 0; u < A[v].size(); u++) {
+
+                                            if (randomValues[v] > randomValues[A[v][u]]) {
+                                                std::lock_guard<std::mutex> lock(mu);
+                                                M_prim.insert(v);
                                             }
+
+
                                         }
 
-                                        if ( add )
-                                        {
-                                            {
-                                                unique_lock<mutex> l{ m };
-                                                M_prim.push_back( v );
-                                            }
-                                        }
                                     }
+
+
                                 }
                         )
                 );
+
             }
 
-            for ( auto &t : threadPool )
+            for (auto &t : threadPool2)
                 t.join();
 
-            threadPool.clear();
-
+            threadPool2.clear();
             // check if M_prim is empty, in that case add the first vertex to be colored
-            if ( M_prim.size() == 0 )
-            {
-                for ( int i = 1; i <= V; i++ )
-                {
-                    if ( A[ i ].size() > 0 )
-                    {
-                        M_prim.push_back( i );
+            if (M_prim.size() == 0) {
+                bool isFound=false;
+                for (int i = 0; i <= V; i++) {
+                    if (A[i].size()>0) {
+                        M_prim.insert(i);
+                        isFound=true;
                         break;
                     }
                 }
+
+
             }
-
+            int size = M_prim.size();
             //add M_prim to M
-            for ( int j = 0; j < M_prim.size(); j++ )
-                M.push_back( M_prim[ j ] );
+            for (int value: M_prim)
+                M.push_back(value);
 
-            _V -= ( int ) M_prim.size();
-            vs -= ( int ) M_prim.size();
-
+            _V -= (int) M_prim.size();
+            vs -= (int) M_prim.size();
+            if(M_prim.size()==0){
+                _V--;
+                vs-=1;
+            }
             //remove from v the set s and all the neighbors in s
 
             //cout << "MIS: {";
-            for ( int i = 0; i < M_prim.size(); i++ )
-            {
-                int index = M_prim[ i ];
-                //cout << index << ", ";
+            set<int, greater<int> >::iterator i;
+            for (i = M_prim.begin(); i != M_prim.end(); i++) {
+                int index = *i;
+
 
                 // removing neighbors of vertex v
-                for ( int j = 0; j < A[ index ].size(); j++ )
-                {
-                    if ( A[ A[ index ][ j ]].size() > 0 )
-                    {
-                        A[ A[ index ][ j ]].erase( A[ A[ index ][ j ]].begin(), A[ A[ index ][ j ]].end());
-                        _V--;
+                for (int j = 0; j < A[index].size(); j++) {
+                    _V--;
+                    if (A[A[index][j]].size() > 0) {
+                        A[A[index][j]].erase(A[A[index][j]].begin(), A[A[index][j]].end());
+
+
                     }
                 }
 
                 // removing the vertex v
-                A[ index ].erase( A[ index ].begin(), A[ index ].end());
-                graph[ index ].erase( graph[ index ].begin(), graph[ index ].end());
+                A[index].erase(A[index].begin(), A[index].end());
+                graph[index].erase(graph[index].begin(), graph[index].end());
             }
+            M_prim.clear();
 
-            //cout << "}" << endl;
+            cout << _V << endl;
+
         }
 
         return M;
     }
 
-    int *colorMIS()
-    {
+    int *colorMIS() {
         // this->graph[ x ].remove( v );
-        int *colors = new int[V + 1];
+        int *colors = new int[V + 1]{-1};
 
         int color = 0;
 
-        while ( vs > 0 )
-        {
+        while (vs > 0) {
             vector<int> mis = this->FindMaximalIndependentSet();
+            int size= mis.size();
+            if (size > 0) {
+                for (int i = 0; i < mis.size(); i++){
+                    colors[mis[i]] = color;
+                }
 
-            if ( mis.size() > 0 )
-            {
-                for ( int i = 0; i < mis.size(); i++ )
-                    colors[ mis[ i ]] = color;
-
-                color++;
+               color+=1;
             }
         }
+        for(int i=1;i<V+1 ; i++){
+            if(colors[i]==-1){
+                colors[i]=color-1;
+            }
+        }
+
+
 
         cout << endl << "Used color: " << color << endl;
 
@@ -248,29 +229,24 @@ public:
 
 };
 
-chrono::steady_clock::time_point getCurrentClock()
-{
+chrono::steady_clock::time_point getCurrentClock() {
     return chrono::steady_clock::now();
 }
 
-void printElapsedTime( chrono::steady_clock::time_point start, chrono::steady_clock::time_point end )
-{
-    printf( "%02ld:%02ld:%02lld.%09lld\n",
-            chrono::duration_cast<chrono::hours>( end - start ).count(),
-            chrono::duration_cast<chrono::minutes>( end - start ).count(),
-            chrono::duration_cast<chrono::seconds>( end - start ).count(),
-            chrono::duration_cast<chrono::nanoseconds>( end - start ).count());
+void printElapsedTime(chrono::steady_clock::time_point start, chrono::steady_clock::time_point end) {
+    printf("%02ld:%02ld:%02lld.%09lld\n",
+           chrono::duration_cast<chrono::hours>(end - start).count(),
+           chrono::duration_cast<chrono::minutes>(end - start).count(),
+           chrono::duration_cast<chrono::seconds>(end - start).count(),
+           chrono::duration_cast<chrono::nanoseconds>(end - start).count());
 }
 
-int main()
-{
-    NTHREAD = thread::hardware_concurrency();
-
+int main() {
     chrono::steady_clock::time_point start_time, start_time_coloring, end_time;
 
-    ifstream file( "/Users/giannicito/Documents/SDP/Course Material/project/gragh-coloring/data/rgg_n_2_15_s0.graph" );
+    ifstream file("/Users/S273284/CLionProjects/untitled/rgg_n_2_15_s0.graph");
 
-    if ( !file.is_open())
+    if (!file.is_open())
         cout << "failed to open file\n";
 
     string line;
@@ -281,21 +257,18 @@ int main()
 
     start_time = getCurrentClock();
 
-    while ( getline( file, line ) && cv <= V )
-    {
+    while (getline(file, line) && cv <= V) {
         // cout << line << endl;
-        stringstream sin( line );
+        stringstream sin(line);
 
-        if ( firstRow )
-        {
+        if (firstRow) {
             firstRow = false;
             sin >> V >> E;
-            G = new Graph( V );
-        } else
-        {
+            G = new Graph(V);
+        } else {
             int e;
-            while ( sin >> e )
-                G->addEdge( cv, e );
+            while (sin >> e)
+                G->addEdge(cv, e);
 
             cv++;
         }
@@ -304,27 +277,27 @@ int main()
     end_time = getCurrentClock();
 
     cout << endl << "Reading time: ";
-    printElapsedTime( start_time, end_time );
+    printElapsedTime(start_time, end_time);
     cout << endl;
 
-    G->print();
+//    G->print();
 
     start_time_coloring = getCurrentClock();
     int *colors = G->colorMIS();
     end_time = getCurrentClock();
 
     cout << endl << "Coloring time: ";
-    printElapsedTime( start_time_coloring, end_time );
+    printElapsedTime(start_time_coloring, end_time);
     cout << endl;
 
 
     // print the result
-    for ( int u = 1; u <= V; u++ )
+    for (int u = 1; u <= V; u++)
         cout << "Vertex " << u << " --->  Color "
-             << colors[ u ] << endl;
+             << colors[u] << endl;
 
     cout << endl << "Total elapsed time: ";
-    printElapsedTime( start_time, end_time );
+    printElapsedTime(start_time, end_time);
     cout << endl;
 
     return 0;
